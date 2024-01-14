7c4f2fa825508089bd7eb1d839fd937acfb33c05
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-14 15:59:02 +0600

    table create for students

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Students from "./components/Students/Students";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="bg-[#172227] font-[Inter] text-white">
         <Header />
         <Hero />
+        <Students></Students>
         <Footer />
       </div>
     </>
