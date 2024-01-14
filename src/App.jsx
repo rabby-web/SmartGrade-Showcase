a4f76c23fc2f25209dc6c059369c0f537b048764
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-14 15:13:52 +0600

    rabby-web/SmartGrade-Showcase

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <>
+      <div className="bg-[#172227] font-[Inter] text-white">
+        <Header />
+        <Hero />
+      </div>
+    </>
+  );
+}
+
+export default App;
