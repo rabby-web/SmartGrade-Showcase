6da731872723e1304752383e8dec9ef62ec8dbc5
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-14 16:44:42 +0600

    class tow table complete

--- src/components/ClassTow/ClassTow.jsx
+++ src/components/ClassTow/ClassTow.jsx
@@ -1,3 +1,5 @@
+import students2 from "../../../public/classTow.json";
+
 export default function ClassTow() {
   return (
     <>
@@ -6,23 +8,31 @@ export default function ClassTow() {
           Class Two
         </td>
       </tr>
-      <tr className="border-b border-[#7ECEB529]">
-        <td className="p-5 text-sm md:text-xl">1</td>
-        <td className="p-5 text-sm md:text-xl">
-          <div className="flex space-x-3 items-center">
-            <img
-              className="w-8 h-8"
-              src="./assets/avatar.png"
-              width="32"
-              height="32"
-              alt="John Smith"
-            />
-            <span className="whitespace-nowrap">John Smith</span>
-          </div>
-        </td>
-        <td className="p-5 text-sm md:text-xl text-center">A+</td>
-        <td className="p-5 text-sm md:text-xl text-center">98%</td>
-      </tr>
+      {students2.map((student) => (
+        <>
+          <tr className="border-b border-[#7ECEB529]">
+            <td className="p-5 text-sm md:text-xl">{student.id}</td>
+            <td className="p-5 text-sm md:text-xl">
+              <div className="flex space-x-3 items-center">
+                <img
+                  className="w-8 h-8 rounded-full"
+                  src={student.image}
+                  width="32"
+                  height="32"
+                  alt="John Smith"
+                />
+                <span className="whitespace-nowrap">{student.name}</span>
+              </div>
+            </td>
+            <td className="p-5 text-sm md:text-xl text-center">
+              {student.score}
+            </td>
+            <td className="p-5 text-sm md:text-xl text-center">
+              {student.percentage}%
+            </td>
+          </tr>
+        </>
+      ))}
     </>
   );
 }
