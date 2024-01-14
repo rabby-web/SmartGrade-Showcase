16c42bac9aba98964ebb070d646e3843f50244d6
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-14 16:31:26 +0600

    class one complete

--- src/components/ClassOne/ClassOne.jsx
+++ src/components/ClassOne/ClassOne.jsx
@@ -1,4 +1,6 @@
+import students1 from "../../../public/classOne.json";
 export default function classNameOne() {
+  console.log(students1);
   return (
     <>
       {/* <!-- className two --> */}
@@ -7,7 +9,32 @@ export default function classNameOne() {
           Class One
         </td>
       </tr>
-      <tr className="border-b border-[#7ECEB529]">
+      {students1.map((student) => (
+        <>
+          <tr className="border-b border-[#7ECEB529]">
+            <td className="p-5 text-sm md:text-xl">{student.id}</td>
+            <td className="p-5 text-sm md:text-xl">
+              <div className="flex space-x-3 items-center">
+                <img
+                  className="w-8 h-8 rounded-full"
+                  src={student.image}
+                  width="32"
+                  height="32"
+                  alt="John Smith"
+                />
+                <span className="whitespace-nowrap">{student.name}</span>
+              </div>
+            </td>
+            <td className="p-5 text-sm md:text-xl text-center">
+              {student.score}
+            </td>
+            <td className="p-5 text-sm md:text-xl text-center">
+              {student.percentage}%
+            </td>
+          </tr>
+        </>
+      ))}
+      {/* <tr className="border-b border-[#7ECEB529]">
         <td className="p-5 text-sm md:text-xl">1</td>
         <td className="p-5 text-sm md:text-xl">
           <div className="flex space-x-3 items-center">
@@ -23,7 +50,7 @@ export default function classNameOne() {
         </td>
         <td className="p-5 text-sm md:text-xl text-center">A+</td>
         <td className="p-5 text-sm md:text-xl text-center">98%</td>
-      </tr>
+      </tr> */}
     </>
   );
 }
